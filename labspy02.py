1ee591e24f20a56ef15555c13412e2a73befaf98
ihsannurulalam <ihsannurulalam276@gmail.com>
2021-11-15 22:51:10 +0700

    hasil ppemrograman

---
+++ labspy02.py
@@ -0,0 +1,18 @@
+print("Ihsan Nurul Alam")
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+
+maks = 0
+
+print(a,b,c)
+if a > b:
+    maks = a
+else:
+    maks = b
+
+if c > maks:
+    maks = c
+
+    print("terbesar:   ",maks)
+    
---
+++ labspy03.py
@@ -0,0 +1,13 @@
+print("Ihsan Nurul Alam")
+bil1=int(input("Masukkan Bilangan = "))
+bil2=int(input("Masukkan BIlangan = "))
+bil3=int(input("Masukkan Bilangan = "))
+
+if(bil1>bil2) and (bil1>bil3):
+    print("Terbesarnya Adalah = ",bil1)
+elif(bil2>bil1) and (bil2>bil3):
+    print("Terbesarnya Adalah= ",bil2)
+else:
+    print("Terbesarnya Adalah= ",bil3)
+    
+    
