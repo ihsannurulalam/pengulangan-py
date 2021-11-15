a8294167164262c204cb4e113b21d3c41083ca7c
ihsannurulalam <ihsannurulalam276@gmail.com>
2021-11-15 23:00:13 +0700

    hasil pemrograman

---
+++ Latihan1.py
@@ -0,0 +1,10 @@
+print("Ihsan Nurul Alam")
+## inport library random
+from random import random
+n = int(input("Masukan Bilangan  :"))
+while n==n:
+    break
+for i in range(n):
+    bil = random()%0.5 #bilangan random kurang dari 0.5 
+    print("Perulangan Ke-  :", bil)
+    print("Selesai")
